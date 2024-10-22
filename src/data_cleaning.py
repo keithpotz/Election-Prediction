1bb640225980d59b0c414fdc5d36eb80c60a6bdd
Keith <keithpotz@gmail.com>
2024-10-22 11:13:18 -0400

    Update to data_cleaning.py
    Updated the items excluded from the cleaning of raw data

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -8,14 +8,16 @@ def clean_polling_data(file_path):
     # 1. Drop irrelevant columns
     irrelevant_columns = ['timestamp', 'comment', 'sponsor_ids', 'pollster_rating_id', 'notes', 'url','pollster_id','pollster','sponsors','display_name','pollster_name','pollster_rating_name','ranked_choice_round',
     'sponsor_candidate_id','sponsor_candidate','sponsor_candidate_party','endorsed_candidate_id','endorsed_candidate_name','endorsed_candidate_party','url_article',
-    'url_topline','url_crosstab','source','internal','partisan','poll_start_date','poll_end_date','methodology','population_full','created_at','poll_start','subpopulation','poll_end_date','poll_start_date','tracking','seat_number']  
+    'url_topline','url_crosstab','source','internal','partisan','methodology','population_full','created_at','subpopulation','tracking','seat_number', 'population']  
     # Add the actual irrelevant columns from the dataset
     data.drop(columns=irrelevant_columns, inplace=True, errors='ignore')
     
     # 2. Handle missing values
     # Drop rows with missing values in key columns like 'candidate_name' and 'pct'
     data.dropna(subset=['candidate_name', 'pct'], inplace=True)
-
+    data['state'].fillna('Unknown', inplace=True)
+    #removes rows where 'Unknown' is present
+    data = data[data['state'] != 'Unkown']
     # Optionally fill remaining missing values with 0
     data.fillna(0, inplace=True)
 
@@ -27,7 +29,7 @@ def clean_polling_data(file_path):
     data.rename(columns=rename_columns, inplace=True)
    
     # 4. Convert date columns to datetime format
-    date_columns = ['poll_start_date', 'poll_end_date']
+    date_columns = ['start_date', 'end_date']
     for col in date_columns:
         if col in data.columns:
             data[col] = pd.to_datetime(data[col], format='%y-%m-%d', errors='coerce') # convert invalid dates like '0' to NaT
@@ -46,7 +48,6 @@ def clean_polling_data(file_path):
 if __name__ == "__main__":
     base_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(base_dir, '../data/polling_data/raw_polls.csv') #Construct path to raw data file
-    cleaned_data = clean_polling_data(file_path)
     cleaned_data = clean_polling_data(file_path)
     print("Polling data cleaned and saved to ../ep/Election-Prediction/data/polling_data/cleaned_polls.csv")
     print(cleaned_data.head())
